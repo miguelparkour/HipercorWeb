64bb5c3b133f4d72b878bcd1d4e619e837c06e48
MiguelParkour <migue.bermu93@gmail.com>
2020-05-02 10:09:27 +0200

    primer controler

---
+++ Controllers/TiendaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HipercorWeb.Controllers
+{
+    public class TiendaController : Controller
+    {
+        #region propiedades de clase
+        #endregion
+        #region metodos de clase
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
---
+++ Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HipercorWeb.Models
+{
+    public class Cliente
+    {
+        #region propiedades de clase
+        public string User { get; set; }
+        public string Password { get; set; }
+        #endregion
+        #region metodos de clase
+        #endregion
+    }
+}
